561eb20101c71d239727a071460c3b57e6e5c2f5
POOJA <POOJA@DESKTOP-AHL9HLQ>
2024-01-11 11:35:12 +0530

    by pooja

---
+++ Git_program2/src/com/cjc/programs/Test.java
@@ -0,0 +1,25 @@
+package com.cjc.programs;
+
+public class Test 
+{
+public void m1()
+{
+	System.out.println("m1 method");
+}
+public void m2()
+{
+	System.out.println("m2 method");
+}
+public void m3()
+{
+	System.out.println("m3 method");
+}
+public void m4()
+{
+	System.out.println("m4 method");
+}
+public void m5()
+{
+	System.out.println("m5 method");
+}
+}
